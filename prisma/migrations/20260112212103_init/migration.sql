8c5bc02b947ce9e3cc23c3cca5b4627bc9bf2afd
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-01-12 22:31:20 +0100

    update : switched to postgreSQL

--- prisma/migrations/20260112212103_init/migration.sql
+++ prisma/migrations/20260112212103_init/migration.sql
@@ -1,17 +1,19 @@
 -- CreateTable
 CREATE TABLE "Photographer" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "city" TEXT NOT NULL,
     "country" TEXT NOT NULL,
     "tagline" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
-    "portrait" TEXT NOT NULL
+    "portrait" TEXT NOT NULL,
+
+    CONSTRAINT "Photographer_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Media" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "photographerId" INTEGER NOT NULL,
     "title" TEXT NOT NULL,
     "image" TEXT,
@@ -19,5 +21,9 @@ CREATE TABLE "Media" (
     "likes" INTEGER NOT NULL,
     "date" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
-    CONSTRAINT "Media_photographerId_fkey" FOREIGN KEY ("photographerId") REFERENCES "Photographer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Media_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "Media" ADD CONSTRAINT "Media_photographerId_fkey" FOREIGN KEY ("photographerId") REFERENCES "Photographer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
